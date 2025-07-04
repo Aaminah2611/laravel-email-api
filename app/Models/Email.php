70c01025e076850a8855b7ef40c0cfe10f83d42e
Aaminah2611 <Aaminah2611@gmail.com>
2025-07-04 16:25:19 +0100

    Set up Laravel project and configured .env for MySQL. Created Email and EmailTemplate models with migrations to define table structure for storing email data and templates. Ran migrations to create corresponding database tables.

---
+++ app/Models/Email.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+    protected $fillable = ['from', 'to', 'subject', 'body', 'status', 'sent_at'];
+}
